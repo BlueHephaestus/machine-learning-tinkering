94ec6345225e7facbf825cf585cfb5fdb320142d
DarkElement75 <blakeedwards@gmail.com>
2016-06-16 02:46:29 -0400

    Backups and softmax testing

--- network_dev_test2.py
+++ network_dev_test2.py
@@ -1,101 +1,138 @@
-
-'''
-run_count = 3
-epochs = 1
-training_data_subsections=10
-'''
-#First, we run our configurations
-import mnist_loader
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-
-import network_dev2
-
-f = open('test_output.txt', 'w').close()
-
 #So we have similarities to use for our graphing / these need to be the same for it to be more or less reasonable
-run_count = 3
+#Basically these are our global things
+configs = 1
+run_count = 1
 epochs = 1
-training_data_subsections=100
+training_data_subsections=50
+comparison_title="Momentum Coefficient comparisons"
+comparison_file="momentum_comparison"
+xaxis_title="Epoch Subsection"
+yaxis_title="MNIST % Accuracy"
+update_output = True
+graph_output = False
+subplots=False
 
-#Subplot 1
-net = network_dev2.Network([784, 30, 10])
-net.SGD(training_data, 
-        epochs, #epochs
-        10,#m
-        3.0,#eta
-        5,#test_accuracy_check_rate
-        2,#eta_decrease_factor
-        .45,#u
-        training_data_subsections=training_data_subsections, 
-        test_data=test_data,
-        run_count=run_count)
+if update_output:
+    #First, we run our configurations
+    import mnist_loader
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    training_data = training_data[:1000]
 
-#Subplot 2
-net = network_dev2.Network([784, 30, 10], weight_init=network_dev2.large_weight_initializer)
-net.SGD(training_data, 
-        epochs, #epochs
-        10,#m
-        3.0,#eta
-        5,#test_accuracy_check_rate
-        2,#eta_decrease_factor
-        .45,#u
-        training_data_subsections=training_data_subsections, 
-        test_data=test_data,
-        run_count=run_count)
+    import network_dev2
 
-#Then we graph the results
-from plotly import tools
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-import numpy as np
-import json
-
-f = open('test_output.txt', 'r')
-traces = []
-
-for config in f.readlines():
-    config_results = json.loads(config)
-    if training_data_subsections:
-        N = epochs*training_data_subsections
-        x = np.linspace(0, N, N)
-        for r in config_results:
-            y = []
-            for j in config_results[r]:
-                for s in config_results[r][j]:
-                    y.append(config_results[r][j][s])
-            trace = go.Scatter(
-                x = x,
-                y = y
-            )
-            traces.append(trace)
-
-    else:
-        N = epochs
-        x = np.linspace(0, N, N)
-        for r in config_results:
-            y = []
-            for j in config_results[r]:
-                y.append(config_results[r][j])
-            trace = go.Scatter(
-                x = x,
-                y = y
-            )
-            traces.append(trace)
+    f = open('{0}_output.txt'.format(comparison_file), 'w').close()
     
-fig = tools.make_subplots(rows=2, cols=4, shared_xaxes=True, shared_yaxes=True) 
-fig.append_trace(traces[0], 1, 1)
-fig.append_trace(traces[1], 1, 2)
-fig.append_trace(traces[2], 1, 3)
-fig.append_trace(traces[3], 1, 4)
-fig.append_trace(traces[4], 2, 1)
-fig.append_trace(traces[5], 2, 2)
-fig.append_trace(traces[6], 2, 3)
-fig.append_trace(traces[7], 2, 4)
-#fig.append_trace(trace2, 2, 1)
-#fig.append_trace(trace3, 2, 2)
-# Plot and embed in ipython notebook!
-#py.iplot(data, filename='basic-line')
-# Open new tab in browser
-plot_url = py.plot(fig, filename='basic-line')
+    config_count = 0
+    net = network_dev2.Network([784, 30, 10], output_filename=comparison_file, softmax=False)
+    net.SGD(training_data, 
+            epochs, #epochs
+            10,#m
+            3.0,#eta
+            5,#test_accuracy_check_interval
+            2,#eta_decrease_factor
+            .2,#u
+            training_data_subsections=training_data_subsections, 
+            test_data=test_data,
+            config_num=config_count,
+            run_count=run_count)
+
+
+if graph_output:
+    #Then we graph the results
+    from plotly import tools
+    import plotly.plotly as py
+    import plotly.graph_objs as go
+
+    import numpy as np
+    import json
+    from collections import OrderedDict
+
+    f = open('{0}_output.txt'.format(comparison_file), 'r')
+    traces = []
+
+    for config in f.readlines():
+        config_results = json.loads(config, object_pairs_hook=OrderedDict)#So we preserve the order of our stored json
+        if training_data_subsections:
+            N = epochs*training_data_subsections
+            x = np.linspace(0, N, N)
+            for r in config_results:
+                y = []
+                for j in config_results[r]:
+                    for s in config_results[r][j]:
+                        y.append(config_results[r][j][s])
+                if int(r) >= run_count:
+                    #Our final, average run
+                    trace = go.Scatter(
+                        x = x,
+                        y = y,
+                        line = dict(
+                            width = 4
+                        ),
+                        name="Average Run",
+                        mode="line"
+                    )
+                else:
+                    trace = go.Scatter(
+                        x = x,
+                        y = y,
+                        line = dict(
+                            width = 1,
+                            dash = "dot"
+                        ),
+                        name="Test Run #{0}".format(r)
+                    )
+                traces.append(trace)
+
+        else:
+            N = epochs
+            x = np.linspace(0, N, N)
+            for r in config_results:
+                y = []
+                for j in config_results[r]:
+                    y.append(config_results[r][j])
+                if int(r) >= run_count:
+                    #Our final, average run
+                    trace = go.Scatter(
+                        x = x,
+                        y = y,
+                        line = dict(
+                            width = 4
+                        ),
+                        name="Average Run",
+                        mode="line"
+                    )
+                else:
+                    trace = go.Scatter(
+                        x = x,
+                        y = y,
+                        line = dict(
+                            width = 1,
+                            dash = "dot"
+                        ),
+                        name="Test Run #{0}".format(r)
+                    )
+                traces.append(trace)
+        
+    if subplots:
+        fig = tools.make_subplots(rows=1, cols=configs, shared_xaxes=True, shared_yaxes=True) 
+        col_num = 1
+        for trace_index, trace in enumerate(traces):
+            if np.mod(trace_index, (len(traces)/configs)) == 0 and trace_index > 0:
+                col_num += 1
+            fig.append_trace(trace, 1, col_num)
+
+        fig["layout"].update(title=comparison_title)
+        for xaxis_num in range(1,configs+1):
+            fig["layout"]["xaxis{0}".format(xaxis_num)].update(title=xaxis_title)
+        fig["layout"]["yaxis1"].update(title=yaxis_title)
+    else:
+        layout = dict(title=comparison_title, xaxis=dict(title=xaxis_title), yaxis=dict(title=yaxis_title),)
+        fig = dict(data=traces, layout=layout)
+
+    plot_url = py.plot(fig, filename='ayy-lmao')
+    #fig.append_trace(trace2, 2, 1)
+    #fig.append_trace(trace3, 2, 2)
+    # Plot and embed in ipython notebook!
+    #py.iplot(data, filename='basic-line')
+    # Open new tab in browser
 
